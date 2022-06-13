7f04cab52e39bc5f28d494435e01c887dd44440c
HALLI Souhaib <71531874+souhaib20@users.noreply.github.com>
2022-06-13 19:22:00 +0100

    Update dataapp.js

--- dataapp.js
+++ dataapp.js
@@ -3,9 +3,9 @@ let users = [
       id: "123456789",
       createdDate: "2021-01-06",
       status: "En validation",
-      firstName: "Mohamed",
-      lastName: "Taha",
-      userName: "mtaha",
+      firstName: "Souhaib",
+      lastName: "halli",
+      userName: "halli",
       registrationNumber: "2584",
     },
     
